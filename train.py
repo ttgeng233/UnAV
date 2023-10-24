b1fb627f3b1bc39f1bcb361ea4ed8e9eb4b6ebf0
ed <edward.fish@surrey.ac.uk>
2023-10-24 14:02:56 +0100

    update for temp maxer

--- train.py
+++ train.py
@@ -90,7 +90,9 @@ def main(args):
     model = make_multimodal_meta_arch(cfg['model_name'], **cfg['model'])
 
     # not ideal for multi GPU training, ok for now
-    model = nn.DataParallel(model, device_ids=cfg['devices'])
+    model = nn.DataParallel(model, device_ids=cfg['devices']) 
+    # if len(cfg['devices']) > 1:
+    #     model = nn.DataParallel(model, device_ids=cfg['devices'])
     # optimizer
     optimizer = make_optimizer(model, cfg['opt'])
     # schedule
